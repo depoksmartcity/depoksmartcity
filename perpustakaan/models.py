ad7f1061e448931faef64a6e9aa808d168ca0428
Muhammad Rizqy Ramadhan <rizqy.r@gmail.com>
2022-10-23 22:53:07 +0700

    feature(perpustakaan): init models

--- perpustakaan/models.py
+++ perpustakaan/models.py
@@ -1,7 +1,7 @@
 from email.policy import default
 from turtle import mode
 from django.db import models
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -18,6 +18,15 @@ class Book(models.Model):
     photo = models.ImageField(upload_to=None)
     stock = models.IntegerField(default=0, validators=[MinValueValidator(0)])
     is_available = models.BooleanField(default=False)
+    rate = models.DecimalField(max_digit=3, decimal_places=2, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    borrowed_times = models.IntegerField(default=0)
 
 class BookReview(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    review = models.TextField()
+
+class BookFavorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    favorite = models.BooleanField(default=False)
