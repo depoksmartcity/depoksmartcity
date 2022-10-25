fb443c1fe8fb601cdfe8d2bc7d90ca940681da0d
Muhammad Rizqy Ramadhan <rizqy.r@gmail.com>
2022-10-25 20:34:13 +0700

    feature(perpustakaan): init views

--- perpustakaan/views.py
+++ perpustakaan/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.urls import reverse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core import serializers
+from .models import Author, Publisher, Book, BookReview, BookFavorite
 
 # Create your views here.
+
+def get_all_author(request):
+    author_data = Author.objects.all()
+    context = {'data': author_data}
+    return
+
+def get_all_author_json(request):
+    author_data = Author.objects.all()
+    context = {'data': author_data}
+    return
+
