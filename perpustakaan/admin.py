5d5d6ba688a56553fedbed4a866fc9a439cd9d3a
Muhammad Rizqy Ramadhan <rizqy.r@gmail.com>
2022-10-25 14:34:35 +0700

    feature(perpustakaan): register model to admin

--- perpustakaan/admin.py
+++ perpustakaan/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Author, Publisher, Book, BookReview, BookFavorite
 
 # Register your models here.
-
+admin.site.register(Author)
+admin.site.register(Publisher)
+admin.site.register(Book)
+admin.site.register(BookReview)
+admin.site.register(BookFavorite)
