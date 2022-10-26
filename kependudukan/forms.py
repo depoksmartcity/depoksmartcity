a5b644892718860a6edf0b4975a6255ea7757d4a
maureenesthw <maureen.esther.wijaya@gmail.com>
2022-10-26 13:25:18 +0700

    add css and edit forms

--- kependudukan/forms.py
+++ kependudukan/forms.py
@@ -1,24 +1,38 @@
 from django import forms
-   
+
+# iterables
+KECAMATAN_CHOICES = (
+    (0, "Beji"),
+    (1, "Pancoran Mas"),
+    (2, "Cipayung"),
+    (3, "Sukmajaya"),
+    (4, "Cilodong"),
+    (5, "Limo"),
+    (6, "Cinere"),
+    (7, "Cimanggis"),
+    (8, "Tapos"),
+    (9, "Sawangan"),
+    (10, "Bojong Sari")
+)
+
 # creating a form 
 class RequestKTPForm(forms.Form):
     provinsi = "Jawa Barat"
     kota = "Depok"
-    kecamatan = forms.ChoiceField()
-    kelurahan = forms.ChoiceField()
+    kecamatan = forms.ChoiceField(choices=KECAMATAN_CHOICES, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'kecamatan'}))
+    kelurahan = forms.CharField(max_length=250, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'kelurahan'}))
 
-    permohonan = forms.CharField(max_length=250)
+    permohonan = forms.CharField(max_length=250, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'permohonan'}))
 
-    nama_lengkap = forms.CharField(max_length = 250)
-    nomor_kk = forms.CharField(max_length=16)
-    nik = forms.CharField(max_length=16)
-    alamat = forms.Textarea()
+    nama_lengkap = forms.CharField(max_length = 250, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'nama_lengkap'}))
+    nomor_kk = forms.CharField(max_length=16, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'nomor_kk'}))
+    nik = forms.CharField(max_length=16, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'nik'}))
+    alamat = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'alamat'}))
 
     rt = forms.IntegerField()
     rw = forms.IntegerField()
-    kode_pos = forms.CharField(max_length=5)
+    kode_pos = forms.CharField(max_length=5, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'kode_pos'}))
 
-    nomor_hp = forms.CharField(max_length=15) # untuk dihubungi dalam melakukan konfirmasi
+    nomor_hp = forms.CharField(max_length=15, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'nomor_hp'})) # untuk dihubungi dalam melakukan konfirmasi
 
-    schedule_date = forms.DateField()
-    schedule_time = forms.TimeField()
+    jadwal = forms.CharField(max_length = 250, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'jadwal'}))
