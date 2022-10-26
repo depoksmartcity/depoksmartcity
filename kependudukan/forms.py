3ef4278cd9269340d79b2da9612d78a74f2ba8e6
maureenesthw <maureen.esther.wijaya@gmail.com>
2022-10-26 14:52:41 +0700

    add more styling and edit form

--- kependudukan/forms.py
+++ kependudukan/forms.py
@@ -2,6 +2,20 @@ from django import forms
 
 # iterables
 KECAMATAN_CHOICES = (
+    ("Beji", "Beji"),
+    ("Pancoran Mas", "Pancoran Mas"),
+    ("Cipayung", "Cipayung"),
+    ("Sukmajaya", "Sukmajaya"),
+    ("Cilodong", "Cilodong"),
+    ("Limo", "Limo"),
+    ("Cinere", "Cinere"),
+    ("Cimanggis", "Cimanggis"),
+    ("Tapos", "Tapos"),
+    ("Sawangan", "Sawangan"),
+    ("Bojong Sari", "Bojong Sari")
+)
+
+KELURAHAN_CHOICES = (
     (0, "Beji"),
     (1, "Pancoran Mas"),
     (2, "Cipayung"),
@@ -15,22 +29,27 @@ KECAMATAN_CHOICES = (
     (10, "Bojong Sari")
 )
 
+PERMOHONAN_CHOICES = (
+    ("Baru", "Baru"),
+    ("Penggantian", "Penggantian")
+)
+
 # creating a form 
 class RequestKTPForm(forms.Form):
     provinsi = "Jawa Barat"
     kota = "Depok"
-    kecamatan = forms.ChoiceField(choices=KECAMATAN_CHOICES, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'kecamatan'}))
-    kelurahan = forms.CharField(max_length=250, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'kelurahan'}))
+    kecamatan = forms.ChoiceField(choices=KECAMATAN_CHOICES, widget=forms.Select(attrs={'class': 'form-control', 'name': 'kecamatan'}))
+    kelurahan = forms.ChoiceField(choices=KELURAHAN_CHOICES, widget=forms.Select(attrs={'class': 'form-control', 'name': 'kelurahan'}))
 
-    permohonan = forms.CharField(max_length=250, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'permohonan'}))
+    permohonan = forms.ChoiceField(choices=PERMOHONAN_CHOICES, widget=forms.Select(attrs={'class': 'form-control', 'name': 'permohonan'}))
 
     nama_lengkap = forms.CharField(max_length = 250, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'nama_lengkap'}))
     nomor_kk = forms.CharField(max_length=16, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'nomor_kk'}))
     nik = forms.CharField(max_length=16, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'nik'}))
-    alamat = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'alamat'}))
+    alamat = forms.CharField(widget=forms.Textarea(attrs={'rows':'5', 'class': 'form-control', 'name': 'alamat'}))
 
-    rt = forms.IntegerField()
-    rw = forms.IntegerField()
+    rt = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control', 'name': 'rt'}))
+    rw = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control', 'name': 'rw'}))
     kode_pos = forms.CharField(max_length=5, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'kode_pos'}))
 
     nomor_hp = forms.CharField(max_length=15, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'nomor_hp'})) # untuk dihubungi dalam melakukan konfirmasi
