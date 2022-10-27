917c2f2fc3295c5584b8e88b30de1ccc4fbe26d6
bagas-sw <bagassw42@gmail.com>
2022-10-27 22:19:05 +0700

    initial commit register page

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, register_user
 
 app_name = 'homepage'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('register/', register_user, name='register'),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,4 +1,27 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.core import serializers
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+import datetime
 
 def index(request):
     return render(request, 'index.html')
+
+def register_user(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('homepage')
+    
+    context = {'form':form}
+    return render(request, 'register.html', context)
