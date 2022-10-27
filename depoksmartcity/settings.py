e970d0ea0dcdb5532438d1c634369c4e48910b72
bagas-sw <bagassw42@gmail.com>
2022-10-27 22:21:51 +0700

    update kesehatan module

--- depoksmartcity/settings.py
+++ depoksmartcity/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'homepage',
     'kependudukan',
     'perpustakaan',
+    'kesehatan',
 ]
 
 MIDDLEWARE = [
--- kesehatan/models.py
+++ kesehatan/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Patient(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    first_name = models.CharField(max_length=30)
+    middle_name = models.CharField(null=True, blank=True, max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+class Hospital(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=15)
+    status = models.BooleanField()
+    sub_district = models.CharField(max_length = 50)
+
+class Puskesmas(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=15)
+    status = models.BooleanField()
+    sub_district = models.CharField(max_length = 50) 
