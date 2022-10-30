6686e26fc11c7927ac1dbb4abc6e67aa48dfee48
ariefbraja <marief.braja@gmail.com>
2022-10-30 20:52:33 +0700

    fix aspirasi.js

---
+++ static/js/aspirasi.js
@@ -0,0 +1,32 @@
+async function getAspirasi(){
+    return fetch("/aspirasi/json").then((res) => res.json())
+  }
+ 
+  async function refreshAspirasi(){
+    document.getElementById("output").innerHTML = ""
+    const aspirasi = await getAspirasi()
+    let htmlString = ``
+    htmlString+=`<p><b>List Aspirasi</b></p>`
+    aspirasi.forEach(item=> {
+      htmlString+=`\n
+      <div class="cards" >
+        <p>
+        <span class="nowrap">${item.fields.aspirasi}</span>
+        </p>
+      </div>
+      `
+    });
+    document.getElementById("output").innerHTML = htmlString
+  }
+  
+  async function addAspirasi() {
+    fetch("{% url 'aspirasi:add_aspirasi_ajax' %}", {
+          method: "POST",
+          body: new FormData(document.querySelector('#postAspirasi'))
+      }).then(refreshAspirasi)
+    return false
+  }
+ 
+ 
+  refreshAspirasi()
+ 
