94537ee5eda3d84bb09dec2b8dcf45b38ec8a9ab
maureenesthw <maureen.esther.wijaya@gmail.com>
2022-10-31 10:03:31 +0700

    add login styling

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import index
+from .views import index, register_user, login_user, logout_user
 
 app_name = 'homepage'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('register/', register_user, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
