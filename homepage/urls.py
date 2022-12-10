8807ff6675329cb5a9124dbfd508995c815a5ee6
maureenesthw <maureen.esther.wijaya@gmail.com>
2022-12-10 17:31:32 +0700

    register-flutter

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, login_user, register_user, logout_user, login_flutter
+from .views import index, login_user, register_user, logout_user, login_flutter, register_flutter
 
 app_name = 'homepage'
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path('register/', register_user, name='register'),
     path('logout/', logout_user, name='logout'),
     path('login-flutter/', login_flutter, name='login_flutter'),
+    path('register-flutter/', register_flutter, name='register_flutter'),
 
 ]
