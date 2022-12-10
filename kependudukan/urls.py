243f1e48c6cb42a623e1f8178b38e14f08d9d901
maureenesthw <maureen.esther.wijaya@gmail.com>
2022-12-10 23:04:39 +0700

    add add-flutter for request KTP

--- kependudukan/urls.py
+++ kependudukan/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from kependudukan.views import show_kependudukan, show_request_ktp, show_json, show_request_ktp_json, add_request, get_kelurahan, show_all_request_ktp_json
+from kependudukan.views import show_kependudukan, show_request_ktp, show_json, show_request_ktp_json, add_request, get_kelurahan, show_all_request_ktp_json, add_request_flutter
 
 app_name = 'kependudukan'
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('request-ktp/json', show_request_ktp_json, name='show_request_ktp_json'),
     path('all-request-ktp/json', show_all_request_ktp_json, name='show_all_request_ktp_json'),
     path('request-ktp/add', add_request, name='add_request'),
+    path('request-ktp/add-flutter', add_request_flutter, name='add_request_flutter'),
 ]
